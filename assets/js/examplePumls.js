36f1e2a1c231317db79b7cb7f12ae760deed13cd
sakirtemel <sakirtemel@users.noreply.github.com>
2023-01-26 06:05:53 +0000

    Update example pumls list

--- assets/js/examplePumls.js
+++ assets/js/examplePumls.js
@@ -1,3 +1,4 @@
 const examplePumls = [
   {url: 'examples/basic/sequence_diagram.puml', label: 'basic/sequence_diagram.puml'},
+  {url: 'examples/community/cloudflare_linode_firewall.puml', label: 'community/cloudflare_linode_firewall.puml'},
 ]
