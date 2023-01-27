0d0bdb00a5f9f742b9fbe9303ce8741e60a8377b
Sakir Temel <sakirtemel@users.noreply.github.com>
2023-01-27 22:04:25 +0000

    Add plantuml as a dependency for teavm

--- teavm/teavm-maven-webapp/src/main/java/org/teavm/Client.java
+++ teavm/teavm-maven-webapp/src/main/java/org/teavm/Client.java
@@ -1,12 +1,21 @@
 package org.teavm;
 
+import java.io.IOException;
+
 import org.teavm.jso.dom.html.HTMLDocument;
 import org.teavm.jso.dom.html.HTMLElement;
+import net.sourceforge.plantuml.wasm.RunEmpty;
+
 
 public class Client {
     public static void main(String[] args) {
         HTMLDocument document = HTMLDocument.current();
         HTMLElement div = document.createElement("div");
+        try {
+            RunEmpty.main(args);
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }        
         div.appendChild(document.createTextNode("TeaVM generated element"));
         document.getBody().appendChild(div);
     }
