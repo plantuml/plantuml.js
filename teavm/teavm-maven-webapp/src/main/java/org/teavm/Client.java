74b0f7b145fb9735518bd68346c05ebab933013a
Sakir Temel <sakirtemel@users.noreply.github.com>
2023-01-27 22:41:35 +0000

    Call plantuml convert from teavm

--- teavm/teavm-maven-webapp/src/main/java/org/teavm/Client.java
+++ teavm/teavm-maven-webapp/src/main/java/org/teavm/Client.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.teavm.jso.dom.html.HTMLDocument;
 import org.teavm.jso.dom.html.HTMLElement;
 import net.sourceforge.plantuml.wasm.RunEmpty;
+// import net.sourceforge.plantuml.wasm.Utils;
 
 
 public class Client {
@@ -13,6 +14,7 @@ public class Client {
         HTMLElement div = document.createElement("div");
         try {
             RunEmpty.main(args);
+            // Utils.convertPng("result.png", "@startuml\nBob -> Alice: Hi\n@enduml\n");
         } catch (IOException e) {
             System.out.println("IOException");
         }        
