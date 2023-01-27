e245048c0c313c751300f373db424cf05131515f
Sakir Temel <sakirtemel@users.noreply.github.com>
2023-01-27 19:51:53 +0100

    Add awslib example (#20)
    * Add awslib example
    
    * Update example pumls list

--- assets/js/examplePumls.js
+++ assets/js/examplePumls.js
@@ -1,4 +1,5 @@
 const examplePumls = [
+  {url: 'examples/stdlib/awslib.puml', label: 'stdlib/awslib.puml'},
   {url: 'examples/basic/sequence_diagram.puml', label: 'basic/sequence_diagram.puml'},
   {url: 'examples/community/cloudflare_linode_firewall.puml', label: 'community/cloudflare_linode_firewall.puml'},
 ]
