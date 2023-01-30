e23a003e96d123f4e02e67ac07e82a8131e10a74
Sakir Temel <sakirtemel@gmail.com>
2023-01-30 02:47:25 +0100

    Remove beta tag

--- compile.sh
+++ compile.sh
@@ -4,12 +4,12 @@ ROOT_DIR=`pwd`
 
 wget https://d3415aa6bfa4.leaningtech.com/cheerpj_linux_2.3.tar.gz -O - | tar -xz
 
-# beta compile
-rm $ROOT_DIR/compiled-beta-jars/*
+# compile
+rm $ROOT_DIR/compiled-jars/*
 
-cp $ROOT_DIR/source-beta-jars/plantuml-wasm.jar $ROOT_DIR/compiled-beta-jars
+cp $ROOT_DIR/source-jars/plantuml-wasm.jar $ROOT_DIR/compiled-jars
 
-cd $ROOT_DIR/compiled-beta-jars
+cd $ROOT_DIR/compiled-jars
 
 $ROOT_DIR/cheerpj_2.3/cheerpjfy.py plantuml-wasm.jar
 
