7525364de9df5bf0dd053a5484146a6a72296bb1
sakirtemel <sakirtemel@users.noreply.github.com>
2023-02-01 08:02:19 +0000

    Update example pumls list

--- playground/assets/examplePumls.js
+++ playground/assets/examplePumls.js
@@ -1,5 +1,5 @@
 const examplePumls = [
-  {url: 'examples/stdlib/awslib.puml', label: 'stdlib/awslib.puml'},
-  {url: 'examples/basic/sequence_diagram.puml', label: 'basic/sequence_diagram.puml'},
-  {url: 'examples/community/cloudflare_linode_firewall.puml', label: 'community/cloudflare_linode_firewall.puml'},
+  {url: 'playground/example-pumls/stdlib/awslib.puml', label: 'stdlib/awslib.puml'},
+  {url: 'playground/example-pumls/basic/sequence_diagram.puml', label: 'basic/sequence_diagram.puml'},
+  {url: 'playground/example-pumls/community/cloudflare_linode_firewall.puml', label: 'community/cloudflare_linode_firewall.puml'},
 ]
