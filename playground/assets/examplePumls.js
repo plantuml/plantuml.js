698717761b1916eaa6a20f2b92233844face4ae9
arnaudroques <arnaudroques@users.noreply.github.com>
2023-02-01 19:58:27 +0000

    Recompile

--- playground/assets/examplePumls.js
+++ playground/assets/examplePumls.js
@@ -2,4 +2,6 @@ const examplePumls = [
   {url: 'playground/example-pumls/stdlib/awslib.puml', label: 'stdlib/awslib.puml'},
   {url: 'playground/example-pumls/basic/sequence_diagram.puml', label: 'basic/sequence_diagram.puml'},
   {url: 'playground/example-pumls/community/cloudflare_linode_firewall.puml', label: 'community/cloudflare_linode_firewall.puml'},
+  {url: 'playground/example-pumls/community/emoji.puml', label: 'community/emoji.puml'},
+  {url: 'playground/example-pumls/community/spacelab_theme.puml', label: 'community/spacelab_theme.puml'},
 ]
