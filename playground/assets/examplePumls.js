5d7888f10b9552de117048513891a8d3d84d4722
sakirtemel <sakirtemel@users.noreply.github.com>
2023-04-03 21:10:58 +0000

    Recompile

--- playground/assets/examplePumls.js
+++ playground/assets/examplePumls.js
@@ -1,7 +1,14 @@
 const examplePumls = [
-  {url: 'playground/example-pumls/stdlib/awslib.puml', label: 'stdlib/awslib.puml'},
-  {url: 'playground/example-pumls/basic/sequence_diagram.puml', label: 'basic/sequence_diagram.puml'},
-  {url: 'playground/example-pumls/community/cloudflare_linode_firewall.puml', label: 'community/cloudflare_linode_firewall.puml'},
-  {url: 'playground/example-pumls/community/emoji.puml', label: 'community/emoji.puml'},
   {url: 'playground/example-pumls/community/spacelab_theme.puml', label: 'community/spacelab_theme.puml'},
+  {url: 'playground/example-pumls/community/emoji.puml', label: 'community/emoji.puml'},
+  {url: 'playground/example-pumls/community/c4_system_landscape.puml', label: 'community/c4_system_landscape.puml'},
+  {url: 'playground/example-pumls/basic/sequence_diagram.puml', label: 'basic/sequence_diagram.puml'},
+  {url: 'playground/example-pumls/c4/system_context.puml', label: 'c4/system_context.puml'},
+  {url: 'playground/example-pumls/c4/system_landscape.puml', label: 'c4/system_landscape.puml'},
+  {url: 'playground/example-pumls/c4/deployment.puml', label: 'c4/deployment.puml'},
+  {url: 'playground/example-pumls/c4/containers.puml', label: 'c4/containers.puml'},
+  {url: 'playground/example-pumls/c4/live_deployment.puml', label: 'c4/live_deployment.puml'},
+  {url: 'playground/example-pumls/c4/dynamic.puml', label: 'c4/dynamic.puml'},
+  {url: 'playground/example-pumls/c4/components.puml', label: 'c4/components.puml'},
+  {url: 'playground/example-pumls/stdlib/awslib.puml', label: 'stdlib/awslib.puml'},
 ]
