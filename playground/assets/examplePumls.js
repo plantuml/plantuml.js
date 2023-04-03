2ea797bdbf87ab89d1535f355ee1bdc11aec4ef1
sakirtemel <sakirtemel@users.noreply.github.com>
2023-04-03 21:13:24 +0000

    Recompile

--- playground/assets/examplePumls.js
+++ playground/assets/examplePumls.js
@@ -1,4 +1,5 @@
 const examplePumls = [
+  {url: 'playground/example-pumls/community/cloudflare_linode_firewall.puml', label: 'community/cloudflare_linode_firewall.puml'},
   {url: 'playground/example-pumls/community/spacelab_theme.puml', label: 'community/spacelab_theme.puml'},
   {url: 'playground/example-pumls/community/emoji.puml', label: 'community/emoji.puml'},
   {url: 'playground/example-pumls/basic/sequence_diagram.puml', label: 'basic/sequence_diagram.puml'},
